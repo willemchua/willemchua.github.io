c9fcc6f9f16a44fcdfb949699ebb32919bc02af6
willemchua <willem.chua@traveloka.com>
2020-04-19 02:10:59 +0700

    Updates

---
+++ precache-manifest.f16f2f9afc100e87029e1b10ae978dd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627c9cdcee2348bd97af13f92a883588",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6147a625103165e0f4d3",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "01b0149e051c1203c94f",
+    "url": "/static/js/2.79e390d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.79e390d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6147a625103165e0f4d3",
+    "url": "/static/js/main.8d8d7c28.chunk.js"
+  },
+  {
+    "revision": "6609c55c3baee5766435",
+    "url": "/static/js/runtime-main.27faa037.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
